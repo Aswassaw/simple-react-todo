0bda9d67144c88d812031cacd4149d365c272518
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-09 11:16:54 +0700

    Finish onChange and onSubmit for Input Component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,30 +1,56 @@
 import "./Input.css";
-import React from "react";
+import React, { useState } from "react";
 
 const Input = () => {
-  const onChange = () => {};
+  const [formData, setFormData] = useState({
+    title: "",
+    note: "",
+  });
 
-  const onSubmit = () => {};
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
 
   return (
     <div>
-      <form onSubmit={onSubmit} className="form">
+      <form onSubmit={onSubmit} className='form'>
         <div>
-          <label htmlFor='name'>Name</label>
+          <label htmlFor='title'>Title</label>
           <br />
-          <input className="form-input" type='text' name='name' onChange={onChange} />
+          <input
+            className='form-input'
+            type='text'
+            name='title'
+            value={formData.title}
+            onChange={onChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor='note'>Note</label>
           <br />
           <textarea
-          className="form-input"
+            className='form-input'
             name='note'
             cols='30'
             rows='10'
+            value={formData.note}
             onChange={onChange}
+            required
           ></textarea>
         </div>
+        <button className='button' type='submit'>
+          SUBMIT
+        </button>
       </form>
     </div>
   );
