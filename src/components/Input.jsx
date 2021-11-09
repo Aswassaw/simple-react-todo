467e4fbea350c7ef984fd9c99880089ab8c4ab8e
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-09 11:06:49 +0700

    Styling Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,33 @@
-import './Input.module.css';
-import React from 'react';
+import "./Input.css";
+import React from "react";
 
 const Input = () => {
+  const onChange = () => {};
+
+  const onSubmit = () => {};
+
   return (
     <div>
-      <input type="text" name="As" id="" />
+      <form onSubmit={onSubmit} className="form">
+        <div>
+          <label htmlFor='name'>Name</label>
+          <br />
+          <input className="form-input" type='text' name='name' onChange={onChange} />
+        </div>
+        <div>
+          <label htmlFor='note'>Note</label>
+          <br />
+          <textarea
+          className="form-input"
+            name='note'
+            cols='30'
+            rows='10'
+            onChange={onChange}
+          ></textarea>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
